f98284499f881a8a7293d684eecc878685efb464
unknown <k25650727@gmail.com>
2025-04-18 11:18:22 +0200

    before nesting productservice folder

--- productservice/src/main/java/com/productservice/productservice/events/ProductDeletedEvent.java
+++ productservice/src/main/java/com/productservice/productservice/events/ProductDeletedEvent.java
@@ -1,12 +1,10 @@
 package com.productservice.productservice.events;
 
-import com.productservice.productservice.models.Product;
-
 public class ProductDeletedEvent {
-    public Product product;
+    public int productId;
 
-    public ProductDeletedEvent(Product product) {
-        this.product = product;
+    public ProductDeletedEvent(int id) {
+        this.productId = id;
     }
 
 }
